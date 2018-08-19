ca571d4d2c4a64840772ad0c8132af305cbca9ac
jboothSV <jbooth@simpleviewinc.com>
2018-08-19 13:57:58 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Playground</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Rajdhani|Righteous" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+
+            <div class="col">
+                <div class="cont">
+                    <div class="square big" id="target">
+                        <div class="square little"></div>
+                        <div class="square little"></div>
+                        <div class="square little"></div>
+                        <div class="square little"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+<script type="text/javascript" src="js/gsap/src/minified/utils/SplitText.min.js"></script>
+<script type="text/javascript" src="js/gsap/src/minified/TweenMax.min.js"></script>
+<script type="text/javascript" src="js/gsap/src/minified/TimelineMax.min.js"></script>
+<script type="text/javascript" src="js/draw.js"></script>
+<script type="text/javascript" src="js/move.js"></script>
+
+</body>
+</html>
